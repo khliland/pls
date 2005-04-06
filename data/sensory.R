05b2d27493052afa74e228d737544fdc58cd7e18
Ron Wehrens <ron.wehrens@gmail.com>
2005-04-06 07:09:17 +0000

    Store data in binary form

--- data/sensory.R
+++
@@ -1,5 +0,0 @@
-### sensory.R: set up the sensory data set
-### $Id$
-
-sensory <- data.frame(Quality = I(as.matrix(read.table("quality"))),
-                      Panel = I(as.matrix(read.table("panel"))))
