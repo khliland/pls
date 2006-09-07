4a3c32378346362ceaa0952b85cc142a611ed625
Bjørn-Helge Mevik <b-h@mevik.net>
2006-09-07 13:47:23 +0000

    simpls.fit.R: Optimized code.  Also standardised variable names

--- R/simpls.fit.R
+++ R/simpls.fit.R
@@ -1,7 +1,10 @@
 ### simpls.fit.R: SimPLS fit algorithm.
-### Adapted from: De Jong, Chemolab v18 (1993) 251-263
-### By Ron Wehrens.  Edited by Bjørn-Helge Mevik.
 ### $Id$
+###
+### Implements an adapted version of the SIMPLS algorithm described in
+###   de Jong, S. (1993) SIMPLS: an alternative approach to partial least
+###   squares regression.  \emph{Chemometrics and Intelligent Laboratory
+###   Systems}, \bold{18}, 251--263.
 
 simpls.fit <- function(X, Y, ncomp, stripped = FALSE, ...)
 {
@@ -11,62 +14,77 @@ simpls.fit <- function(X, Y, ncomp, stripped = FALSE, ...)
         dnX <- dimnames(X)
         dnY <- dimnames(Y)
     }
-    ## Remove dimnames during calculation.  This can save time!
-    dimnames(X) <- dimnames(Y) <- NULL
+    ## Remove dimnames during calculation (doesn't seem to matter; in fact,
+    ## as far as it has any effect, it hurts a tiny bit in most situations).
+    ## dimnames(X) <- dimnames(Y) <- NULL
 
     nobj <- dim(X)[1] # n in paper
-    nvar <- dim(X)[2] # p in paper
-    npred <- dim(Y)[2]
+    npred <- dim(X)[2] # p in paper
+    nresp <- dim(Y)[2]
+
+    V <- R <- matrix(0, nrow = npred, ncol = ncomp)
+    tQ <- matrix(0, nrow = ncomp, ncol = nresp) # Y loadings; transposed
+    B <- array(0, dim = c(npred, nresp, ncomp))
+    if (!stripped) {
+        P <- R
+        U <- TT <- matrix(0, nrow = nobj, ncol = ncomp)
+        fitted <- array(0, dim = c(nobj, nresp, ncomp))
+    }
 
     ## Center variables:
     Xmeans <- colMeans(X)
-    X <- sweep(X, 2, Xmeans)            # This is not strictly neccessary
+    X <- colsubtract(X, Xmeans)         # This is not strictly neccessary
                                         # (but might be good for accuracy?)!
     Ymeans <- colMeans(Y)
-    Y <- sweep(Y, 2, Ymeans)
-  
+    Y <- colsubtract(Y, Ymeans)
+
     S <- crossprod(X, Y)
-    RR <- matrix(0, ncol = ncomp, nrow = nvar)
-    QQ <- matrix(0, ncol = ncomp, nrow = npred)
-    TT <- matrix(0, ncol = ncomp, nrow = nobj)
-    VV <- matrix(0, ncol = ncomp, nrow = nvar)
-    B <- array(0, c(dim(X)[2], dim(Y)[2], ncomp))
-    if(!stripped) {
-        PP <- matrix(0, ncol = ncomp, nrow = nvar)
-        UU <- matrix(0, ncol = ncomp, nrow = nobj)
-        Ypred <- array(0, c(nobj, npred, ncomp))
-    }
 
     for (a in 1:ncomp) {
-        qq <- svd(S)$v[,1]              # Y block factor weights
-        rr <- S %*% qq                  # X block factor weights
-        tt <- X %*% rr
-        tt <- tt - mean(tt)             # center scores
-        tnorm <- sqrt(sum(tt*tt))
-        tt <- tt / tnorm                # normalize scores
-        rr <- rr / tnorm                # adapt weights accordingly
-        pp <- crossprod(X, tt)          # X block factor loadings
-        qq <- crossprod(Y, tt)          # Y block factor loadings
-        uu <- Y %*% qq                  # Y block factor scores
-        vv <- pp			# init orthogonal loadings
-        if (a > 1){
-            vv <- vv - VV %*% crossprod(VV, pp) # vv orth to previous loadings
-            uu <- uu - TT %*% crossprod(TT, uu) # uu orth to previous tt values
+        ## A more efficient way of calculating the Y block factor weights
+        ## q.a <- svd(S)$v[,1]:
+        if (nresp == 1) {
+            q.a <- 1
+        } else {
+            if (nresp < npred) {
+                q.a <- eigen(crossprod(S), symmetric = TRUE)$vectors[,1]
+            } else {
+                q.a <- c(crossprod(S, eigen(S %*% t(S),
+                                            symmetric = TRUE)$vectors[,1]))
+                q.a <- q.a / sqrt(c(crossprod(q.a)))
+            }
         }
-        vv <- vv / sqrt(sum(vv * vv))  # normalize orthogonal loadings
-        S <- S - vv %*% crossprod(vv, S) # deflate S
-    
-        RR[,a] <- rr
-        TT[,a] <- tt
-        QQ[,a] <- qq
-        VV[,a] <- vv
+        r.a <- S %*% q.a                 # X block factor weights
+        t.a <- X %*% r.a
+        t.a <- t.a - mean(t.a)           # center scores
+        tnorm <- sqrt(c(crossprod(t.a)))
+        t.a <- t.a / tnorm               # normalize scores
+        r.a <- r.a / tnorm               # adapt weights accordingly
+        p.a <- crossprod(X, t.a)         # X block factor loadings
+        q.a <- crossprod(Y, t.a)         # Y block factor loadings
+        v.a <- p.a			 # init orthogonal loadings
+        if (a > 1) {
+            v.a <- v.a - V %*% crossprod(V, p.a) # v.a orth to previous loadings
+        }
+        v.a <- v.a / sqrt(c(crossprod(v.a))) # normalize orthogonal loadings
+        S <- S - v.a %*% crossprod(v.a, S) # deflate S
 
-        B[,,a] <- RR[,1:a, drop=FALSE] %*% t(QQ[,1:a, drop=FALSE])
+        R[,a] <- r.a
+        tQ[a,] <- q.a
+        V[,a] <- v.a
+
+        B[,,a] <- R[,1:a, drop=FALSE] %*% tQ[1:a,, drop=FALSE]
 
         if (!stripped) {
-            PP[,a] <- pp
-            UU[,a] <- uu
-            Ypred[,,a] <- X %*% B[,,a]
+            u.a <- Y %*% q.a # Y block factor scores
+            if (a > 1)
+                u.a <- u.a - TT %*% crossprod(TT, u.a) # u.a orth to previous t.a values
+            P[,a] <- p.a
+            TT[,a] <- t.a
+            U[,a] <- u.a
+            ## (For very tall, slim X and Y, X %*% B[,,a] is slightly faster,
+            ## due to less overhead.)
+            fitted[,,a] <- TT[,1:a] %*% tQ[1:a,, drop=FALSE]
         }
     }
 
@@ -74,31 +92,31 @@ simpls.fit <- function(X, Y, ncomp, stripped = FALSE, ...)
         ## Return as quickly as possible
         list(coefficients = B, Xmeans = Xmeans, Ymeans = Ymeans)
     } else {
-        residuals <- - Ypred + c(Y)
-        Ypred <- sweep(Ypred, 2, Ymeans, "+") # Add mean
+        residuals <- - fitted + c(Y)
+        fitted <- colsubtract(fitted, -Ymeans) # Add mean
 
-        ## Add dimnames:
+        ## Add dimnames and classes:
         objnames <- dnX[[1]]
         if (is.null(objnames)) objnames <- dnY[[1]]
-        xvarnames <- dnX[[2]]
-        yvarnames <- dnY[[2]]
+        prednames <- dnX[[2]]
+        respnames <- dnY[[2]]
         compnames <- paste("Comp", 1:ncomp)
         nCompnames <- paste(1:ncomp, "comps")
-        dimnames(TT) <- dimnames(UU) <- list(objnames, compnames)
-        dimnames(RR) <- dimnames(PP) <- list(xvarnames, compnames)
-        dimnames(QQ) <- list(yvarnames, compnames)
-        dimnames(B) <- list(xvarnames, yvarnames, nCompnames)
-        dimnames(Ypred) <- dimnames(residuals) <-
-            list(objnames, yvarnames, nCompnames)
-        class(TT) <- class(UU) <- "scores"
-        class(PP) <- class(QQ) <- "loadings"
+        dimnames(TT) <- dimnames(U) <- list(objnames, compnames)
+        dimnames(R) <- dimnames(P) <- list(prednames, compnames)
+        dimnames(tQ) <- list(compnames, respnames)
+        dimnames(B) <- list(prednames, respnames, nCompnames)
+        dimnames(fitted) <- dimnames(residuals) <-
+            list(objnames, respnames, nCompnames)
+        class(TT) <- class(U) <- "scores"
+        class(P) <- class(tQ) <- "loadings"
 
         list(coefficients = B,
-             scores = TT, loadings = PP,
-             Yscores = UU, Yloadings = QQ,
-             projection = RR,
+             scores = TT, loadings = P,
+             Yscores = U, Yloadings = t(tQ),
+             projection = R,
              Xmeans = Xmeans, Ymeans = Ymeans,
-             fitted.values = Ypred, residuals = residuals,
-             Xvar = colSums(PP^2), Xtotvar = sum(X^2))
+             fitted.values = fitted, residuals = residuals,
+             Xvar = colSums(P * P), Xtotvar = sum(X * X))
     }
 }
